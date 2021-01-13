387b403568f655b6a57700ff9fc338d374a5de04
NetherXiaoYu <2031464675@qq.com>
2021-01-13 21:55:48 +0800

    更新宝塔测试版7.5.33

--- BTPanel/linuxpanel-develop.sh
+++ BTPanel/linuxpanel-develop.sh
@@ -1,7 +1,7 @@
-wget http://dg1.bt.cn/install/update/LinuxPanel-7.5.31.zip
-unzip LinuxPanel-7.5.31.zip
+wget http://dg1.bt.cn/install/update/LinuxPanel-7.5.33.zip
+unzip LinuxPanel-7.5.33.zip
 cd panel
 bash update.sh
 cd ..
-rm -f LinuxPanel-7.5.31.zip
+rm -f LinuxPanel-7.5.33.zip
 rm -rf panel
