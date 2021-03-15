b287246a56605ef3aa164ff7e13a7ebef764866f
NetherXiaoYu <2031464675@qq.com>
2021-03-15 22:31:40 +0800

    更新寶塔最新版本

--- BTPanel/linuxpanel-develop.sh
+++ BTPanel/linuxpanel-develop.sh
@@ -1,7 +1,7 @@
-wget http://dg1.bt.cn/install/update/LinuxPanel-7.6.10.zip
-unzip LinuxPanel-7.6.10.zip
+wget http://dg1.bt.cn/install/update/LinuxPanel-7.6.15.zip
+unzip LinuxPanel-7.6.15.zip
 cd panel
 bash update.sh
 cd ..
-rm -f LinuxPanel-7.6.10.zip
+rm -f LinuxPanel-7.6.15.zip
 rm -rf panel
--- BTPanel/linuxpanel-stable.sh
+++ BTPanel/linuxpanel-stable.sh
@@ -1,7 +1,7 @@
-wget http://dg1.bt.cn/install/update/LinuxPanel-7.4.71.zip
-unzip LinuxPanel-7.4.7.zip
+wget http://dg1.bt.cn/install/update/LinuxPanel-7.5.1.zip
+unzip LinuxPanel-7.5.1.zip
 cd panel
 bash update.sh
 cd ..
-rm -f LinuxPanel-7.4.7.zip
+rm -f LinuxPanel-7.5.1.zip
 rm -rf panel
